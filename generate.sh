d04c1ffcb47f08277669c04264427edb923902a7
Gabriele Rundo <gabriele.rundo@studenti.unimi.it>
2023-11-30 23:51:01 +0100

    Modifica script e aggiunta .gitignore

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-echo -e "--- \ntitle: Formalizzazione di dati sensibili  \nauthor: Gabriele Rundo \ndate: 30 novembre \ngeometry: left=3cm,right=3cm,top=2cm,bottom=2cm, a4paper\n---" > intro.md
+echo -e "--- \ntitle: Formalizzazione di dati sensibili  \nauthor: Gabriele Rundo \ndate: `date  +"%d %B %Y"` \ngeometry: left=3cm,right=3cm,top=2cm,bottom=2cm, a4paper\n---" > intro.md
 
 pandoc intro.md README.md -o README.pdf
